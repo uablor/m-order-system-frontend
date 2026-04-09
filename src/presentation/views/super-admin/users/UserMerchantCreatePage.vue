926f4233c4f6df6b0f27c37a671e0726066c6698
kongmong <kongmongthao348@gmail.com>
2026-04-09 05:45:53 -0700

    update code push

--- src/presentation/views/super-admin/users/UserMerchantCreatePage.vue
+++ src/presentation/views/super-admin/users/UserMerchantCreatePage.vue
@@ -64,6 +64,7 @@
               v-model:value="formState.contactEmail"
               :placeholder="$t('users.contactEmailPlaceholder')"
               size="large"
+              style="border: 1px solid #d9d9d9; box-shadow: 0 0 0 2px rgba(24, 144, 255, 0.2);"
             />
           </a-form-item>
 
